78e6b2fc8f790a584317481ae602d0b2f1d93f42
albertomp5 <79515564+albertomp5@users.noreply.github.com>
2021-07-08 18:03:05 +0200

    Update UploadFiles.vue

--- web-app/vue-frontend-web/src/components/UploadFiles.vue
+++ web-app/vue-frontend-web/src/components/UploadFiles.vue
@@ -45,7 +45,7 @@
         <v-carousel-item
           v-for="(file, index) in fileInfos"
           :key="index"
-          :src="'http://gbapp.ddns.net:4000/static/usuarios/' + file.name"
+          :src="'https://gbapp.ddns.net/static/usuarios/' + file.name"
           contain
           reverse-transition="fade-transition"
           transition="fade-transition"
